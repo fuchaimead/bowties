87bf1b5eb14f1a732d25c390bac83dbe457f4596
FuChai Mead <mead.fuchai@gmail.com>
2018-02-05 23:44:22 -0700

    aloha collection

--- src/components/Aloha.js
+++ src/components/Aloha.js
@@ -1,15 +1,109 @@
 import React from 'react';
-import Sidebar from './Sidebar'
+import Sidebar from './Sidebar';
+import blue_monstera from '../images/blue_monstera.jpg'
+import blue_seabirds from '../images/blue_seabirds.jpg'
+import brown_palm_tree from '../images/brown_palm_tree.jpg'
+import green_island_floral from '../images/green_island_floral.jpg'
+import red_abstract_floral from '../images/red_abstract_floral.jpg'
+import red_emblem from '../images/red_emblem.jpg'
+import red_floral from '../images/red_floral.jpg'
+import peony_aloha_shirt from '../images/peony_aloha_shirt.jpg'
+import yellow_leaves from '../images/yellow_leaves.jpg'
+
 
 class Aloha extends React.Component {
+  state = { products: [
+    {
+      name: 'Blue Monstera Gardenia Aloha Shirt',
+      price: 149.99,
+      category: 'aloha',
+      sale: true,
+      img: blue_monstera
+    },
+    {
+      name: 'Blue Seabirds and Fish Aloha Shirt',
+      price: 39.99,
+      category: 'aloha',
+      sale: false,
+      img: blue_seabirds
+    },
+    {
+      name: 'Brown Palm Tree Aloha Shirt',
+      price: 49.99,
+      category: 'aloha',
+      sale: true,
+      img: brown_palm_tree
+    },
+    {
+      name: 'Green Island Floral Aloha',
+      price: 12.99,
+      category: 'aloha',
+      sale: true,
+      img: green_island_floral
+    },
+    {
+      name: 'Red Abstract Floral Aloha',
+      price: 29.99,
+      category: 'aloha',
+      sale: false,
+      img: red_abstract_floral
+    },
+    {
+      name: 'Red Emblem Aloha Shirt',
+      price: 18.99,
+      category: 'aloha',
+      sale: false,
+      img: red_emblem
+    },
+    {
+      name: 'Red Floral Aloha Shirt',
+      price: 28.99,
+      category: 'aloha',
+      sale: false,
+      img: red_floral
+    },
+    {
+      name: 'Red Peony Aloha Shirt',
+      price: 49.99,
+      category: 'aloha',
+      sale: false,
+      img: peony_aloha_shirt
+    },
+    {
+      name: 'Yellow Leaves Aloha',
+      price: 59.99,
+      category: 'aloha',
+      sale: true,
+      img: yellow_leaves
+    }, ]
+  }
+
+  displayProducts = () =>{
+    return this.state.products.map( (product, i) => {
+      return (
+      <div className='item' key={i}>
+       <p> {product.name} </p> 
+       <img src={product.img} alt={product.name}/>
+      </div> 
+      )
+      }
+    )
+  }
+
   render() {
     return(
-      <div>
-      <Sidebar /> 
-        this is the Aloha Component
+      <div> 
+      <div> 
+      <h1>  ALOHA COLLECTION </h1> 
       </div>
+      <Sidebar /> 
+      <div className='content'> 
+      { this.displayProducts() }
+      </div> 
+      </div> 
     )
   }
 }
+
 
 export default Aloha
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,81 +1,75 @@
 import React from 'react';
 import Sidebar from './Sidebar';
-import blue_monstera from '../images/blue_monstera.jpg'
-import blue_seabirds from '../images/blue_seabirds.jpg'
-import brown_palm_tree from '../images/brown_palm_tree.jpg'
-import green_island_floral from '../images/green_island_floral.jpg'
-import red_abstract_floral from '../images/red_abstract_floral.jpg'
-import red_emblem from '../images/red_emblem.jpg'
-import red_floral from '../images/red_floral.jpg'
-import peony_aloha_shirt from '../images/peony_aloha_shirt.jpg'
-import yellow_leaves from '../images/yellow_leaves.jpg'
+import alphabet from '../images/alphabet.jpg'
+import asanoha from '../images/asanoha.jpg'
+import hamachidori from '../images/hamachidori.jpg'
+import hyotan_gourd from '../images/hyotan_gourd.jpg'
+import mameshibori from '../images/mameshibori.jpg'
+import orizuru from '../images/orizuru.jpg'
+import samehada from '../images/samehada.jpg'
+import seigaiha from '../images/seigaiha.jpg'
 
 
 class Home extends React.Component {
   state = { products: [
     {
-      name: 'Blue Monstera Gardenia Aloha Shirt',
-      price: 149.99,
-      category: 'aloha',
-      sale: true,
-      img: blue_monstera
-    },
-    {
-      name: 'Blue Seabirds and Fish Aloha Shirt',
-      price: 39.99,
-      category: 'aloha',
-      sale: false,
-      img: blue_seabirds
-    },
-    {
-      name: 'Brown Palm Tree Aloha Shirt',
-      price: 49.99,
-      category: 'aloha',
-      sale: true,
-      img: brown_palm_tree
-    },
-    {
-      name: 'Green Island Floral Aloha',
-      price: 12.99,
-      category: 'aloha',
-      sale: true,
-      img: green_island_floral
-    },
-    {
-      name: 'Red Abstract Floral Aloha',
-      price: 29.99,
-      category: 'aloha',
-      sale: false,
-      img: red_abstract_floral
-    },
-    {
-      name: 'Red Emblem Aloha Shirt',
-      price: 18.99,
-      category: 'aloha',
-      sale: false,
-      img: red_emblem
-    },
-    {
-      name: 'Red Floral Aloha Shirt',
-      price: 28.99,
-      category: 'aloha',
-      sale: false,
-      img: red_floral
-    },
-    {
-      name: 'Red Peony Aloha Shirt',
-      price: 49.99,
-      category: 'aloha',
-      sale: false,
-      img: peony_aloha_shirt
-    },
-    {
-      name: 'Yellow Leaves Aloha',
-      price: 59.99,
-      category: 'aloha',
-      sale: true,
-      img: yellow_leaves
-    }, ]
+            name: 'Alphabet Pattern',
+            price: 129.99,
+            category: 'tenugui',
+            sale: false,
+            article: 'jacket',
+            img: alphabet
+          },
+          {
+            name: 'Asanoha Hemp Leaf Pattern',
+            price: 80.99,
+            category: 'tenugui',
+            sale: false,
+            img: asanoha
+          },
+          {
+            name: 'Hamachidori Birds on the Shore Pattern',
+            price: 59.99,
+            category: 'tenugui',
+            sale: false,
+            img: hamachidori
+          },
+          {
+            name: 'Hyotan Gourd Pattern',
+            price: 59.99,
+            category: 'tenugui',
+            sale: true,
+            img: hyotan_gourd
+          },
+          {
+            name: 'Mameshibori Pea-Dot Pattern',
+            price: 59.99,
+            category: 'tenugui',
+            sale: false,
+            img: mameshibori
+          },
+          {
+            name: 'Orizuru Origami Crane Pattern',
+            price: 59.99,
+            category: 'tenugui',
+            sale: false,
+            img: orizuru
+          },
+          {
+            name: 'Samehada Shark Skin Pattern',
+            price: 59.99,
+            category: 'tenugui',
+            sale: true,
+            img: samehada
+          },
+          {
+            name: 'Seigaiha Wave Pattern',
+            price: 59.99,
+            category: 'tenugui',
+            sale: false,
+            img: seigaiha
+          },
+        ]
   }
 
   displayProducts = () =>{
@@ -93,6 +87,9 @@ class Home extends React.Component {
   render() {
     return(
       <div> 
+      <div> 
+      <h1>  TENUGUI COLLECTION </h1> 
+      </div>
       <Sidebar /> 
       <div className='content'> 
       { this.displayProducts() }
